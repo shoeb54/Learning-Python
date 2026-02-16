b2a697520a174a589c6b580a311ba59e9992abf8
Md. Shoeb Bin Zaman <shoebzaman1999@gmail.com>
2026-02-17 00:56:11 +0600

    Cleaned Code

--- Basics.py
+++ Basics.py
@@ -1,58 +1,105 @@
-# This is my first program
+"""
+Basic Python Practice Program
+Covers:
+- Printing
+- Data types
+- Type casting
+- User input
+- Math operations
+- Conditional statements
+"""
 
-print("Hello world!")
-print("This is my first python program!")
+# -----------------------------------
+# 1. Printing Output
+# -----------------------------------
 
-# Python Variables (Integers, Floats, Strings, Booleans)
+print("Hello World!")
+print("This is my first Python program!\n")
 
-x = 10
-y = 2.6
-charName = 'Hamza'
-charAge = True
 
-print(type(x))
-print(type(y))
-print(type(charName))
-print(type(charAge))
+# -----------------------------------
+# 2. Python Variables & Data Types
+# -----------------------------------
 
-print(f"Hello {charName} You are {x} years old!")
+# Integer
+age_number = 10
 
-#Typecasting means to covert one data type to another 
+# Float
+height = 2.6
 
-likeINt  = int(y+1)
+# String
+character_name = "Hamza"
 
-print(type(likeINt))
-print(y)
+# Boolean
+is_student = True
 
-#User_input is always a string, for example:
+# Display variable types
+print(type(age_number))
+print(type(height))
+print(type(character_name))
+print(type(is_student))
 
-isInput = int(input("Enter a number: "))
-print(f"The number is {isInput}")
+# Using formatted strings (f-strings)
+print(f"\nHello {character_name}, you are {age_number} years old!\n")
 
-# Arithmatic Operators +,-,*,/,%,**
-# friends = 0
-# friend+-**/%
-# print(friends)
 
-#Some math related functions like round(),abs(),pow(),max()
+# -----------------------------------
+# 3. Type Casting
+# -----------------------------------
 
-# import math print(math.pi), math.e, math.sqrt(),math.ceil(),math.floor()
+# Type casting means converting one data type into another
+converted_int = int(height + 1)
 
-# Lets make a code to identify the area of a circle
+print(type(converted_int))
+print(height, "\n")
 
+
+# -----------------------------------
+# 4. User Input
+# -----------------------------------
+
+# Note: input() always returns a string
+user_number = int(input("Enter a number: "))
+print(f"The number is {user_number}\n")
+
+
+# -----------------------------------
+# 5. Math Operations
+# -----------------------------------
+
+# Arithmetic Operators:
+# +  (Addition)
+# -  (Subtraction)
+# *  (Multiplication)
+# /  (Division)
+# %  (Modulus)
+# ** (Exponent)
+
+# Some useful math functions:
+# round(), abs(), pow(), max(), min()
+
+# Using the math module
 import math
+
+
+# -----------------------------------
+# 6. Area of a Circle Calculator
+# -----------------------------------
+
 radius = float(input("Enter the radius of a circle: "))
 
-area = math.pi * pow(radius,2)
+area = math.pi * pow(radius, 2)
 
-print(f"The area of the circle is {round(area, 2)} sqrcm\n")
+print(f"The area of the circle is {round(area, 2)} sq cm\n")
 
-# Conditional Statement : if
 
-age = int(input("Enter your age: "))
+# -----------------------------------
+# 7. Conditional Statement (if-else)
+# -----------------------------------
 
-if age >= 18:
+user_age = int(input("Enter your age: "))
+
+if user_age >= 18:
     print("You are eligible to sign up.")
-
-else :
-    print("You have to 18+ to sign up!")    
+else:
+    print("You must be 18+ to sign up.")
