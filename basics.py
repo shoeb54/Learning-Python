1502b41e16b76d45de54cc3c7f08983e3d760a67
Md. Shoeb Bin Zaman <shoebzaman1999@gmail.com>
2026-02-16 20:05:32 +0600

    Basics Operation

--- basics.py
+++ basics.py
@@ -17,7 +17,7 @@ print(type(charAge))
 
 print(f"Hello {charName} You are {x} years old!")
 
-#Typecasting means to covert one data type to another
+#Typecasting means to covert one data type to another 
 
 likeINt  = int(y+1)
 
@@ -28,3 +28,21 @@ print(y)
 
 isInput = int(input("Enter a number: "))
 print(f"The number is {isInput}")
+
+#Ex-01 Rectangle are calc
+
+length = int(input("Enter the lenght: "))
+width = int(input("Enter the width: "))
+area = length * width
+print(f"The area is {area} cm²")
+
+#Ex02 Shopping Cart Pogram
+print("-----------RUET Market------------")
+item = input("What item would you like to buy? ")
+price = float(input("What is the price of the item? "))
+quantity = int(input("How many would you like? "))
+total = price  * quantity
+
+print(f"You have bought {quantity} x {item}\s.")
+print(f"Your total price is {total} tk only. Thanks for coming.")
+
